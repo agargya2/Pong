2ca68d3007fb71d3159ba9918c87fffa05182c63
Aniket Gargya <gargya.aniket@gmail.com>
2019-02-10 17:29:31 -0600

    Paddles move

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 var canvas;
 var canvasContext;
 
-var ballX;
-var ballY;
-var ballXSpeed;
-var ballYSpeed;
-var ballRadius = 10;
+var ball = {
+  x : 0,
+  y : 0,
+  xSpeed : 3,
+  ySpeed : 3,
+  radius : 10
+}
+
+var paddle1 = {
+  x : 0,
+  y : 0,
+  height : 100,
+  width : 10
+}
+
+var paddle2 = {
+  x : 0,
+  y : 0,
+  height : 100,
+  width: 10,
+  speed : 2
+}
 
 function drawRectangle(coordinateX, coordinateY, width, height, color){
 	canvasContext.fillStyle = color;
@@ -20,36 +37,79 @@ function drawCircle(coordinateX, coordinateY, radius, color){
 }
 
 function initializeGame(){
-  ballX = canvas.width / 2;
-  ballY = canvas.height / 2;
-  ballXSpeed = 1;
-  ballYSpeed = 1;
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
+
+  paddle2.x = canvas.width - paddle2.width;
 }
 
 function moveBall(){
-  ballX += ballXSpeed;
-  ballY += ballYSpeed;
+  ball.x += ball.xSpeed;
+  ball.y += ball.ySpeed;
+}
+
+function bounceOffWall(){
+  if (ball.x - (ball.radius / 2) <= 0 || ball.x + (ball.radius / 2) >= canvas.width){
+    ball.xSpeed *= -1;
+  }
+  if (ball.y - (ball.radius / 2) <= 0 || ball.y + (ball.radius / 2) >= canvas.height){
+    ball.ySpeed *= -1;
+  }
+}
+
+function movePaddle2(){
+  if(paddle2.y + (paddle2.height * 3 / 4) < ball.y){
+    paddle2.y += paddle2.speed;
+  }
+  else if(paddle2.y + (paddle2.height / 4) > ball.y){
+    paddle2.y -= paddle2.speed;
+  }
+}
+
+function mouseToPaddle(evt){
+  var mouse = getMouseLocation(evt);
+  paddle1.y = mouse.y - (paddle1.height / 2);
+}
+
+function getMouseLocation(evt){
+  var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	var mouseX = evt.clientX - rect.left - root.scrollLeft;
+	var mouseY = evt.clientY - rect.top - root.scrollTop;
+	return {x: mouseX, y: mouseY};
 }
 
 function draw(){
   //background
   drawRectangle(0, 0, canvas.width, canvas.height, "black");
 
-  drawCircle(ballX, ballY, ballRadius, "red");
+  //ball
+  drawCircle(ball.x, ball.y, ball.radius, "red");
+
+  //paddle1
+  drawRectangle(paddle1.x, paddle1.y, paddle1.width, paddle1.height, "white");
+
+  //paddle2
+  drawRectangle(paddle2.x, paddle2.y, paddle2.width, paddle2.height, "white");
 }
 
 function move(){
   moveBall();
+  bounceOffWall();
+  movePaddle2();
 }
 
 function update(){
-  draw();
   move();
+  draw();
 }
 
 window.onload = function(){
   canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
+
   initializeGame();
-  setInterval(update, 1000/30)
+  setInterval(update, 1000/300);
+
+  canvas.addEventListener('mousemove', mouseToPaddle);
 }
