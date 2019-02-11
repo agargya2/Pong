f0a34737e1fc1fe97652e83eb5e8a3fdd9c3c9b0
Aniket Gargya <gargya.aniket@gmail.com>
2019-02-10 21:08:24 -0600

    FInished Game with Messed up reflections

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var ball = {
   x : 0,
   y : 0,
   xSpeed : 3,
-  ySpeed : 3,
+  ySpeed : 1,
   radius : 10
 }
 
@@ -13,7 +13,9 @@ var paddle1 = {
   x : 0,
   y : 0,
   height : 100,
-  width : 10
+  width : 10,
+  score : 0,
+  name : "Player"
 }
 
 var paddle2 = {
@@ -21,7 +23,9 @@ var paddle2 = {
   y : 0,
   height : 100,
   width: 10,
-  speed : 2
+  speed : 3,
+  score : 0,
+  name : "Computer"
 }
 
 function drawRectangle(coordinateX, coordinateY, width, height, color){
@@ -36,11 +40,19 @@ function drawCircle(coordinateX, coordinateY, radius, color){
 	canvasContext.fill();
 }
 
+function drawText(coordinateX, coordinateY, size, color, font, text){
+  canvasContext.font = size + " " + font;
+  canvasContext.fillStyle = color;
+  canvasContext.textAlign = "center";
+  canvasContext.fillText(text, coordinateX, coordinateY);
+}
+
 function initializeGame(){
   ball.x = canvas.width / 2;
   ball.y = canvas.height / 2;
 
   paddle2.x = canvas.width - paddle2.width;
+  paddle2.y = (canvas.height / 2) - (paddle2.height / 2)
 }
 
 function moveBall(){
@@ -48,13 +60,39 @@ function moveBall(){
   ball.y += ball.ySpeed;
 }
 
-function bounceOffWall(){
-  if (ball.x - (ball.radius / 2) <= 0 || ball.x + (ball.radius / 2) >= canvas.width){
-    ball.xSpeed *= -1;
+function bounceBall(){
+  if (ball.x - (ball.radius / 2) <= 0){
+    if(paddle1.y - ball.radius < ball.y && paddle1.y + paddle1.height + ball.radius > ball.y){
+      ball.xSpeed *= -1;
+      ball.ySpeed = ((paddle1.y + (paddle1.height / 2)) - ball.y) / (paddle1.height / 2) * 4;
+      console.log(ball.ySpeed);
+    }
+    else{
+      resetBall();
+      paddle2.score++;
+    }
+  }
+  if (ball.x + (ball.radius / 2) >= canvas.width) {
+    if(paddle2.y - ball.radius < ball.y && paddle2.y + paddle2.height + ball.radius > ball.y){
+      ball.xSpeed *= -1;
+      ball.ySpeed = ((paddle2.y + (paddle2.height / 2)) - ball.y) / (paddle2.height / 2) * 4;
+      console.log(ball.ySpeed);
+    }
+    else{
+      resetBall();
+      paddle1.score++;
+    }
   }
   if (ball.y - (ball.radius / 2) <= 0 || ball.y + (ball.radius / 2) >= canvas.height){
     ball.ySpeed *= -1;
   }
+}
+
+function resetBall(){
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
+  ball.xSpeed = Math.random() + 1;
+  ball.ySpeed = Math.random() + 1;
 }
 
 function movePaddle2(){
@@ -79,28 +117,50 @@ function getMouseLocation(evt){
 	return {x: mouseX, y: mouseY};
 }
 
+function drawNet(){
+  var lineLength = 20;
+  var lineWidth = 2;
+  var i = lineLength / 2;
+  while(i < canvas.height){
+    drawRectangle((canvas.width / 2) - lineWidth, i, lineWidth, lineLength, "white");
+    i += (lineLength * 2);
+  }
+}
+
 function draw(){
   //background
   drawRectangle(0, 0, canvas.width, canvas.height, "black");
-
-  //ball
-  drawCircle(ball.x, ball.y, ball.radius, "red");
 
   //paddle1
   drawRectangle(paddle1.x, paddle1.y, paddle1.width, paddle1.height, "white");
 
   //paddle2
   drawRectangle(paddle2.x, paddle2.y, paddle2.width, paddle2.height, "white");
+
+  //ball
+  drawCircle(ball.x, ball.y, ball.radius, "red");
+
+  //scores
+  drawText(canvas.width / 4, canvas.height / 4, "30px", "white", "Arial", paddle1.score);
+  drawText(canvas.width * 3 / 4, canvas.height / 4, "30px", "white", "Arial", paddle2.score);
+
+  drawNet();
+
+  if(paddle2.score >= 3 || paddle1.score >= 3){
+    drawText(canvas.width / 2, canvas.height / 2, "50px", "red", "Arial", ((paddle2.score >= 3) ? paddle2.name : paddle1.name) + " has won the Game!");
+  }
 }
 
 function move(){
   moveBall();
-  bounceOffWall();
+  bounceBall();
   movePaddle2();
 }
 
 function update(){
-  move();
+  if(paddle2.score < 3 && paddle1.score < 3){
+    move();
+  }
   draw();
 }
 
